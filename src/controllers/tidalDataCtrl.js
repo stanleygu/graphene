bfe52161e54c9e9e93d9cef22b2f7b7f896ad0a2
Stanley Gu <stanleygu@gmail.com>
2014-03-05 17:35:36 +0000

    tidal template is responsive in height and can be passed json

--- src/controllers/tidalDataCtrl.js
+++ src/controllers/tidalDataCtrl.js
@@ -1,56 +1,67 @@
 'use strict';
 
 angular.module('sg.nodegraph')
-  .controller('sgTidalDataCtrl', function($scope, $http) {
+  .controller('sgTidalDataCtrl', function($scope, $http, $timeout) {
     if ($scope.jsonUrl) {
       $http.get($scope.jsonUrl).success(function(data) {
-        $scope.ngModel = data;
-        $scope.edges = _.map(data.edges, function(edge) {
-          return {
-            source: _.find(data.nodes, function(n) {
-              return n.id === edge[0];
-            }),
-            target: _.find(data.nodes, function(n) {
-              return n.id === edge[1];
-            }),
-          };
-        });
-
-        var sections = data.timeSlots;
-
-        $scope.groups = [];
-        var count = 0;
-        _.each(sections, function(sect, key) {
-          var nodes = _.filter(data.nodes, function(n) {
-            return _.contains(sect, n.id);
-          });
-          _.each(nodes, function(n) {
-            n.group = count;
-          });
-          var links = _.filter($scope.edges, function(l) {
-            return _.contains(sect, l.source.id) && _.contains(sect, l.target.id);
-          });
-          $scope.forceLayout(nodes, [], {
-            bounds: {
-              w: 800,
-              h: 500,
-            },
-            size: 30
-          });
-          $scope.groups.push({
-            nodes: nodes,
-            links: links,
-            name: key
-          });
-          count += 1;
-        });
-
-        $scope.additionalData = {
-          groups: $scope.groups,
-          edges: $scope.edges
-        };
-
+        $scope.data = data;
       });
+    } else if ($scope.json) {
+      $scope.data = $scope.json;
     }
 
+    // Timeout to let forceLayout function to be available
+    $timeout(function() {
+      $scope.$watch('data', function(newVal) {
+        if (newVal) {
+          var data = newVal;
+          $scope.edges = _.map(data.edges, function(edge) {
+            return {
+              source: _.find(data.nodes, function(n) {
+                return n.id === edge[0];
+              }),
+              target: _.find(data.nodes, function(n) {
+                return n.id === edge[1];
+              }),
+            };
+          });
+
+          var sections = data.timeSlots;
+
+          $scope.groups = [];
+          var count = 0;
+          _.each(sections, function(sect, key) {
+            var nodes = _.filter(data.nodes, function(n) {
+              return _.contains(sect, n.id);
+            });
+            _.each(nodes, function(n) {
+              n.group = count;
+            });
+            var links = _.filter($scope.edges, function(l) {
+              return _.contains(sect, l.source.id) && _.contains(sect, l.target.id);
+            });
+            $scope.forceLayout(nodes, [], {
+              bounds: {
+                w: 800,
+                h: 500,
+              },
+              size: 30
+            });
+            $scope.groups.push({
+              nodes: nodes,
+              links: links,
+              name: key
+            });
+            count += 1;
+          });
+
+          $scope.additionalData = {
+            groups: $scope.groups,
+            edges: $scope.edges
+          };
+
+        }
+      }, 200);
+
+    });
   });
