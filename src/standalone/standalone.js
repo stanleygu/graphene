fd1bb0541832b53788d72f54ff39b80fd5e53050
Stanley Gu <stanleygu@gmail.com>
2014-04-03 21:35:59 +0000

    Removed automatic bootstraping of graphene

--- src/standalone/standalone.js
+++ src/standalone/standalone.js
@@ -1,6 +1,1 @@
 angular.module('graphene-standalone', ['template/sbml.html', 'template/tidal.html', 'sg.graphene']);
-angular.element(document).ready(function() {
-  'use strict';
-  angular.module('myApp', ['graphene-standalone']);
-  angular.bootstrap(document.querySelector('div'), ['myApp']);
-});
