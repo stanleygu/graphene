4e80306197f7e5d2799df8d4a825ef26a1c73ab4
Stanley Gu <stanleygu@gmail.com>
2014-06-04 00:26:40 +0000

    Added ng-xlink-href, ng-width, and ng-height directives

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('sg.graphene', [])
+angular.module('sg.graphene', ['Svgs'])
   .config(['$compileProvider',
     function($compileProvider) {
       // blobs need to be whitelisted for img and a tags
@@ -9,3 +9,43 @@ angular.module('sg.graphene', [])
       $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|tel|file|blob):/);
     }
   ]);
+
+var app = angular.module('Svgs', []);
+
+angular.forEach([{
+  ngAttrName: 'ngXlinkHref',
+  attrName: 'xlink:href'
+}, {
+  ngAttrName: 'ngWidth',
+  attrName: 'width'
+}, {
+  ngAttrName: 'ngHeight',
+  attrName: 'height'
+}], function(pair) {
+
+  var ngAttrName = pair.ngAttrName;
+  var attrName = pair.attrName;
+
+  app.directive(ngAttrName, function() {
+
+    return {
+
+      priority: 99,
+
+      link: function(scope, element, attrs) {
+
+        attrs.$observe(ngAttrName, function(value) {
+
+          if (!value) {
+            return;
+          }
+
+          attrs.$set(attrName, value);
+          //if (IeHelperSrv.isIE) {
+          //  element.prop(attrName, value);
+          //}
+        });
+      }
+    };
+  });
+});
